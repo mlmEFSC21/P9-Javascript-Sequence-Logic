bfa65e49ac4885a3419738896884a9fe89e7e35f
Your Nammeyer.matthew@titans.easternflorida.edue <you@example.com>
2022-02-22 18:10:17 -0500

    commit

--- .history/index_20220222175624.js
+++ .history/index_20220222175624.js
@@ -3,33 +3,33 @@ let second_input = prompt("Please input the first number");
 let x = parseInt(first_input);
 let y = parseInt(second_input);
 
-function add(n1, n2) {
-    return n1 + n2;
+function add(p1, p2) {
+    return p1 + p2;
 }
-function subtract(n1, n2) {
-    return n1 - n2;
+function subtract(p1, p2) {
+    return p1 - p2;
 }
-function multiply(n1, n2) {
+function multiply(p1, p2) {
     1;
-    return n1 * n2;
+    return p1 * p2;
 }
-function average(n1, n2) {
-    return (n1 + n2) / 2;
+function divide(p1, p2) {
+    return p1 / p2;
 }
 
 let sum = add(x, y);
 let difference = subtract(x, y);
 let product = multiply(x, y);
-let avg = average(x, y);
+let quotient = divide(x, y);
 
 console.log(sum);
 console.log(difference);
 console.log(product);
-console.log(avg);
+console.log(quotient);
 
 document.open();
 document.write("<p>The sum of " + x + " and " + y + " is " + sum + "</p>");
 document.write("<p>The difference of " + x + " and " + y + " is " + difference + "</p>");
 document.write("<p>The product of " + x + " and " + y + " is " + product + "</p>");
-document.write("<p>The average of " + x + " and " + y + " is " + avg + "</p>");
-document.close();
+document.write("<p>The quotient of " + x + " and " + y + " is " + quotient + "</p>");
+document.close;
--- .history/index_20220222180721.js
+++ .history/index_20220222180721.js
@@ -16,20 +16,24 @@ function multiply(n1, n2) {
 function average(n1, n2) {
     return (n1 + n2) / 2;
 }
+// function divide(n1, n2) {
+//     return n1 / n2;
+// }
 
 let sum = add(x, y);
 let difference = subtract(x, y);
 let product = multiply(x, y);
-let avg = average(x, y);
+let average = null;
+// let quotient = divide(x, y);
 
 console.log(sum);
 console.log(difference);
 console.log(product);
-console.log(avg);
+console.log(average);
 
 document.open();
 document.write("<p>The sum of " + x + " and " + y + " is " + sum + "</p>");
 document.write("<p>The difference of " + x + " and " + y + " is " + difference + "</p>");
 document.write("<p>The product of " + x + " and " + y + " is " + product + "</p>");
-document.write("<p>The average of " + x + " and " + y + " is " + avg + "</p>");
-document.close();
+document.write("<p>The average of " + x + " and " + y + " is " + average + "</p>");
+document.close;
