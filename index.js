54d26b225d4c113451d0bd7bda516adf11f98344
Your Nammeyer.matthew@titans.easternflorida.edue <you@example.com>
2022-02-22 20:08:00 -0500

    JS successfully takes 2 inputs from prompts, calls 4 functions to do math, adds a <p> to the DOM and inserts the required text into the <p>

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 let first_input = prompt("Please input the first number");
-let second_input = prompt("Please input the first number");
+let second_input = prompt("Please input the second number");
 let x = parseInt(first_input);
 let y = parseInt(second_input);
 
@@ -10,7 +10,6 @@ function subtract(n1, n2) {
     return n1 - n2;
 }
 function multiply(n1, n2) {
-    1;
     return n1 * n2;
 }
 function average(n1, n2) {
@@ -22,14 +21,15 @@ let difference = subtract(x, y);
 let product = multiply(x, y);
 let avg = average(x, y);
 
-console.log(sum);
-console.log(difference);
-console.log(product);
-console.log(avg);
+const para = document.createElement("p");
+// prettier-ignore
+para.innerText = "The sum of " + x + " and " + y + " is " + sum + ".\n" 
++ "The difference of " + x + " and " + y + " is " + difference + ".\n"
++ "The product of " + x + " and " + y + " is " + product + ".\n"
++ "The average of " + x + " and " + y + " is " + avg + ".";
+document.body.appendChild(para);
 
-document.open();
-document.write("<p>The sum of " + x + " and " + y + " is " + sum + "</p>");
-document.write("<p>The difference of " + x + " and " + y + " is " + difference + "</p>");
-document.write("<p>The product of " + x + " and " + y + " is " + product + "</p>");
-document.write("<p>The average of " + x + " and " + y + " is " + avg + "</p>");
-document.close();
+// document.write("<p>The sum of " + x + " and " + y + " is " + sum + "</p>");
+// document.write("<p>The difference of " + x + " and " + y + " is " + difference + "</p>");
+// document.write("<p>The product of " + x + " and " + y + " is " + product + "</p>");
+// document.write("<p>The average of " + x + " and " + y + " is " + avg + "</p>");
